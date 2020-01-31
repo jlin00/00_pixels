7a54ed21a3e0366b02e753e4b55904e9e4ea211d
Jackie Lin <jlin00@stuy.edu>
2020-01-31 15:27:06 -0500

    added file

--- picmaker.c
+++ picmaker.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
 int main(){
+  int fd = open("image.ppm", O_CREAT | O_WRONLY, 0644);
+  if (fd == -1){
+    printf("%s\n", strerror(errno));
+  }
+  int i;
+  int j;
+  for (i = 0; i < 500; i++){
+    for (j = 0; j < 500; j++){
+      /*
+      write(fd, input, strlen(input));
+      if (errno != 0){
+        printf("%s\n", strerror(errno));
+      }
+      */
+    }
+  }
+  close(fd);
   return 0;
 }
