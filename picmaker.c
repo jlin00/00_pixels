1055db3736ae7b8b070e61969174b5f09451bd26
Jackie Lin <jlin00@stuy.edu>
2020-02-02 23:23:38 -0500

    changed to use file * and also added code to make image

--- picmaker.c
+++ picmaker.c
@@ -4,24 +4,27 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <math.h>
 
 int main(){
-  int fd = open("image.ppm", O_CREAT | O_WRONLY, 0644);
-  if (fd == -1){
+  FILE * fp = fopen("image.ppm", "w");
+  if (errno != 0){
     printf("%s\n", strerror(errno));
   }
-  int i;
-  int j;
+  fprintf(fp, "P3\n500 500\n255\n"); //header
+  int i, j;
+  double frequency = 0.01;
+  int width = 25;
+  int center = 230;
   for (i = 0; i < 500; i++){
     for (j = 0; j < 500; j++){
-      /*
-      write(fd, input, strlen(input));
-      if (errno != 0){
-        printf("%s\n", strerror(errno));
-      }
-      */
+      double shift = (j / 25 + 1) * 0.75;
+      int red = sin(frequency * i - shift) * width + center;
+      int green = sin(frequency * i + 2 - shift) * width + center;
+      int blue = sin(frequency * i + 4 - shift) * width + center;
+      fprintf(fp, "%d %d %d\n", red, green, blue);
     }
   }
-  close(fd);
+  fclose(fp);
   return 0;
 }
